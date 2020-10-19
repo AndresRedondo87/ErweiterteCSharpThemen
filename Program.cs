6e21943285eb90ab26417195081fd834d01fc91c
AndresRedondo87 <66962177+AndresRedondo87@users.noreply.github.com>
2020-10-19 12:54:03 +0200

    "Aus Datei Lesen" ABER NUR TXT!! kleine erweiterung hinzugefügt: keine Leelre Linie anzeigen

--- Program.cs
+++ Program.cs
@@ -10,8 +10,30 @@ namespace ErweiterteCSharpThemen
     {
         static void Main(string[] args)
         {
+            // Pfad und Name exact richtig geschrieben, @ für Verbatim formatierungen, die escapezeichen "\" werden ignoriert, damit die Lesbarkeit verbessert wird.
+            // könnte man auch mit doppel zeichen schreiben, ist aber oft eine fehlerquelle "//"
+            // Aufpassen mit pfad nicht verwechseln und kein . anstatt\ zu verwenden.
+            // Manchmal gibt es probleme mit Administrator und schreibrechte. Dagegen könnte helfen, der VisualStudio als Admin auszuführen!!
 
+            //ReadAllText - zeigt das ganze Text
+            string text = System.IO.File.ReadAllText(@"C:\Users\Andres Redondo\source\repos\ErweiterteCSharpThemen\textFile.txt");
+            Console.WriteLine("Via ReadAllText");
+            Console.WriteLine($"Die Textdatei beinhaltet folgender Text: \n{text}");
+            Console.WriteLine("\nEnde der Textdatei\n\n");
 
+            //ReadAllLines, zeigt jede einzelne Zeile (wenn wir via foreach alle Linien aufrufen)
+            string[]  zeilen = System.IO.File.ReadAllLines(@"C:\Users\Andres Redondo\source\repos\ErweiterteCSharpThemen\textFile.txt");
+            Console.WriteLine("Via ReadAllLines, foreach line loop (+Erw.keine Leelre Linie anzeigen):");
+            foreach (string line in zeilen)
+            {
+                //kleine erweiterung um Leere Linien zu überspringen:
+                if (line.Length>0)
+                {
+                    Console.WriteLine(line);
+                }
+                //kein else, einfach weiterlesen
+            }
+            Console.WriteLine("\nEnde der Textdatei\n\n");
 
 
             Console.ReadKey();
@@ -19,3 +41,4 @@ namespace ErweiterteCSharpThemen
         }
     }
 }
+
